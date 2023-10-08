193fd4659fddaf71294c680984c1cdba2a4987e6
Abhinav <abhinavprajapati2001@gmail.com>
2023-10-09 04:23:31 +0530

    Refactor code and remove pid

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,16 @@
 #include <PID_v1.h>
 #include "RPi_Pico_TimerInterrupt.h"
 #include "Commands.h"
-#include "Bts.h"
+#include "BTS7960.h"
 #include "Cytron_MD30C.h"
-
-#define _TIMERINTERRUPT_LOGLEVEL_ 4
-#define _CPU_STATS_ 0
 
 #define _LOG_MOTOR_PWM_ 0
 #define _LOG_ENCODER_TICK_RPM_ 0
 #define _LOG_PID_STATS_ 0
+
+// Specify motor Driver
+#define _BTS7960_
+// #define _Motor_cytron_md30_
 
 // PPR of motor
 #define PPR 1300
@@ -23,14 +24,8 @@
 #define ENCODER_3_IN1 21
 #define ENCODER_3_IN2 20
 
-#define EncoderPinA 3 // 28
-#define EncoderPinB 2 // 27
-
-double Setpoint, Input, Output;
-
-double Kp = 0.01;
-double Ki = 0.01;
-double Kd = 0.01;
+#define ENCODER_1_IN1 28
+#define ENCODER_1_IN2 27
 
 volatile int32_t counts = 0;
 volatile int32_t previous_encoder_ticks = 0;
@@ -45,12 +40,22 @@ struct BusData
 };
 struct BusData DriveData = {0, 0, 0, 0};
 
-// BTS7890 motor_2(6, 7);
-// BTS7890 motor_3(8, 9);
-// BTS7890 motor_1(12, 14);
+#ifdef _BTS7960_
 
-Motor_cytron_md30c motor(6, 7);
-PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+BTS7890 motor_1(6, 7);
+BTS7890 motor_2(8, 9);
+BTS7890 motor_3(12, 14);
+
+#endif
+
+#ifdef _Motor_cytron_md30_
+
+Motor_cytron_md30c motor_1(6, 7);
+Motor_cytron_md30c motor_2(8, 9);
+Motor_cytron_md30c motor_3(12, 14);
+
+#endif
+
 RPI_PICO_Timer ITimer(0);
 
 bool TimerHandler_Calculate_RPM(struct repeating_timer *t)
@@ -104,8 +109,8 @@ void readEncoder()
   static int8_t prevA = 0;
   static int8_t prevB = 0;
 
-  int8_t currA = digitalRead(EncoderPinA);
-  int8_t currB = digitalRead(EncoderPinB);
+  int8_t currA = digitalRead(ENCODER_1_IN1);
+  int8_t currB = digitalRead(ENCODER_1_IN2);
 
   // Determine the direction of rotation
   int8_t direction = (prevA ^ currB) - (prevB ^ currA);
@@ -124,11 +129,11 @@ void setup()
   Serial.setTimeout(1000);
   Serial.begin(115200);
 
-  pinMode(EncoderPinA, INPUT_PULLUP); // Use internal pull-up resistors
-  pinMode(EncoderPinB, INPUT_PULLUP);
+  pinMode(ENCODER_1_IN1, INPUT_PULLUP); // Use internal pull-up resistors
+  pinMode(ENCODER_1_IN2, INPUT_PULLUP);
 
-  attachInterrupt(digitalPinToInterrupt(EncoderPinA), readEncoder, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(EncoderPinB), readEncoder, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_1_IN1), readEncoder, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_1_IN2), readEncoder, CHANGE);
 }
 
 // CORE 1 Main Loop
@@ -140,13 +145,10 @@ void loop()
     run_commands(cmd);
   }
 
-  motor.setPWM(DriveData.motor_1);
+  motor_1.setPWM(DriveData.motor_1);
+  motor_2.setPWM(DriveData.motor_2);
+  motor_3.setPWM(DriveData.motor_3);
 
-  // motor_1.setPWM(DriveData.motor_1 / 4);
-  // motor_2.setPWM(DriveData.motor_2 / 4);
-  // motor_3.setPWM(DriveData.motor_3 / 4);
-  Serial.println("Hello robocon");
-  delay(100);
 #if (_LOG_MOTOR_PWM_)
   Serial.println(DriveData.motor_1);
   Serial.println(DriveData.motor_2);
@@ -157,28 +159,17 @@ void loop()
 // RP2040 Second core
 void setup1()
 {
-  // turn the PID on
-
-  // Attach the timer interrupt with a 50ms interval
   if (ITimer.attachInterruptInterval(RPM_TIMER_INTERVAL_MS * 1000, TimerHandler_Calculate_RPM))
     ;
   // Serial.println(millis());
   else
     Serial.println(F("Can't set Timer Callback. Select another Timer, freq. or timer"));
-
-  // Input = counts;
-  // Setpoint = 25000;
-  // myPID.SetMode(AUTOMATIC);
 }
 
 void loop1()
 {
   // Serial.println(counts / 4);
   // Serial.println(RPM);
-
-  // Input = counts;
-  // myPID.Compute();
-  // motor.setPWM(-Output);
 
 #if (_LOG_PID_STATS_)
   uint32_t time = millis();
